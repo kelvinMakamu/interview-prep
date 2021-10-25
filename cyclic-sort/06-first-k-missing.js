9919247d5ca3309db837b7c4563219d5fecb3d7a
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-10-25 18:16:31 +0300

    Modifies method to get first k missing elements, to include additional edge test cases

--- cyclic-sort/06-first-k-missing.js
+++ cyclic-sort/06-first-k-missing.js
@@ -1,9 +1,6 @@
 /* Given an unsorted array containing numbers and a number ‘k’, find the first ‘k’ missing positive numbers in the array. */
 
 const find_first_missing_posistive = (arr,k) => {
-    if(arr.length < 1){
-        return;
-    }
     let total_elements = arr.length <= k ? arr.length + k : arr.length;
     let smallest_found_positives = {};
     let result = [];
@@ -21,6 +18,8 @@ const find_first_missing_posistive = (arr,k) => {
     return result;
 }
 
+console.log(find_first_missing_posistive([],2));
+console.log(find_first_missing_posistive([1],2));
 console.log(find_first_missing_posistive([3,-1,4,5,5],3));
 console.log(find_first_missing_posistive([2,3,4],3));
 console.log(find_first_missing_posistive([-2,-3,4],2));
